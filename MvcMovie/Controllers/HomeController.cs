a9ad6648d2ddeace1770bd14fa93feac23a8eb10
Vladyslav Potapenko <nylestroke@gmail.com>
2022-05-27 20:25:35 +0200

    Adding files * alpha version

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+using MvcMovie.Services;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly MovieService _movieService;
+
+        public HomeController(ILogger<HomeController> logger, MovieService movieService)
+        {
+            _logger = logger;
+            _movieService = movieService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (string.IsNullOrEmpty(Request.Query["search"]))
+            {
+                ViewData["search"] = null;
+                var result = await this._movieService.GetMovies();
+                return View(result);   
+            }
+            else
+            {
+                ViewData["search"] = Request.Query["search"].ToString();
+                var result = await this._movieService.GetMovies(Request.Query["search"]);
+                return View(result);
+            }
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [Route("/movie/{id}")]
+        public async Task<IActionResult> Movie(int id)
+        {
+            var result = await this._movieService.GetMovie(id);
+            ViewData["id"] = id;
+            return View("Movie", result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
